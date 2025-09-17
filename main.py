d9c95f4f8cf882fd5062bb0d979d1694beb51b54
Nzx03 <nzx786@gmail.com>
2025-09-18 02:05:18 +0530

    data validation

--- main.py
+++ main.py
@@ -3,16 +3,30 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
-
+from src.pipeline.stage2_data_validation import DataValidationTrainingPipeline
 from src.pipeline.stage1_data_ingestion import DataIngestionTrainingPipeline
 from src.logging import logging
 import sys
 from src.exception import CustomException
+
+
+
 STAGE_NAME="Data Ingestion stage"
 try:
     logging.info(f"{STAGE_NAME} started")
     data_ingestion= DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logging.info(f"{STAGE_NAME} completed")
+
+except Exception as e:
+    raise CustomException(e,sys)
+
+
+STAGE_NAME="Data Validation stage"
+try:
+    logging.info(f"{STAGE_NAME} started")
+    data_validation= DataValidationTrainingPipeline()
+    data_validation.main()
     logging.info(f"{STAGE_NAME} completed")
 
 except Exception as e:
--- src/config/configuration.py
+++ src/config/configuration.py
@@ -1,6 +1,6 @@
 from src.constants import *
 from src.utils.modular import read_yaml_file,create_dir
-from src.entity import (DataIngestionConfig)
+from src.entity import (DataIngestionConfig, DataValidationConfig)
 
 
 class ConfigurationManager:
@@ -28,4 +28,17 @@ class ConfigurationManager:
           )
 
           return data_ingestion_config
+    
+    def get_data_validation_config(self)-> DataValidationConfig:
+        config=self.config.data_validation
+         
+        create_dir([config.root_dir])
+
+        data_validation_config=DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE= config.STATUS_FILE,
+            ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES,
+
+        )
+        return data_validation_config
           
